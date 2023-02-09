19323ffb38754c663f0815e0c4673c404ac22cf9
Roydon <2100650G@student.tp.edu.sg>
2023-02-10 07:27:12 +0800

    727 Roydon

--- EsplanadeEventBooking/Shared/Domain/Bookevent.cs
+++ EsplanadeEventBooking/Shared/Domain/Bookevent.cs
@@ -7,14 +7,26 @@ using System.ComponentModel.DataAnnotations;
 
 namespace EsplanadeEventBooking.Shared.Domain
 {
-    public class Bookevent : BaseDomainModel
+    public class Bookevent : BaseDomainModel, IValidatableObject
     {
 		public string Title { get; set; }
 		public DateTime StartDate { get; set; }
-		public DateTime EndDate { get; set; }
+		public DateTime? EndDate { get; set; }
 		public string Location { get; set; }
 		//public Blob Thumbnail { get; set; }
 		public int CreatorId { get; set; }
 		public virtual Creator Creator { get; set; }
-	}
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            //throw new NotImplementedException();
+            if (EndDate != null)
+            {
+                if (EndDate < StartDate)
+                {
+                    yield return new ValidationResult("End Date must not be lesser than Start Date", new[] { "EndDate" });
+                }
+            }
+        } 
+    }
 }
