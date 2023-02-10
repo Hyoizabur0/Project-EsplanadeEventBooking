1db79e2cda533bf99a1d79ce85117bc3cf55ffcd
Roydon <2100650G@student.tp.edu.sg>
2023-02-10 10:00:21 +0800

    Workig 10am

--- EsplanadeEventBooking/Shared/Domain/Ticket.cs
+++ EsplanadeEventBooking/Shared/Domain/Ticket.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,13 +8,14 @@ using System.Threading.Tasks;
 namespace EsplanadeEventBooking.Shared.Domain
 {
     public class Ticket : BaseDomainModel
-    {
-		public int RowNo { get; set; }
-		public int ColumnNo { get; set; }
+	{
+		public int? RowNo { get; set; }
+		public int? ColumnNo { get; set; }
 		public bool VIP { get; set; }
 		public int EuserId { get; set; }
 		public virtual Euser Euser { get; set; }
 		public int BookeventId { get; set; }
 		public virtual Bookevent Bookevent { get; set; }
-	}
+
+    }
 }
